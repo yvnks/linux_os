85eaf84df50385bd4088904f863316ea76b174ca
Jeff Yankson <jjyankson19@gmail.com>
2025-09-12 16:23:25 +0000

    awk & sed

---
+++ APPENDIX D/awk.sh
@@ -0,0 +1,14 @@
+# awk is the appropriate style to print content on the cli.
+# cat means concatenate and it's a style of convention.
+
+awk '{print;}' host.deny | tail 
+
+# Here's how i will print all the lines that are prefixed with a '#' with awk 
+awk '$0~/^#/' host.deny | head 
+
+# awk also has an alternate for the cut command.
+awk '{print$2}' list-ip.sh | tail 
+
+# Another of i would print out the content that do not have '#' prefixed as their first character is:
+awk '$0!~/^#/' host.deny 
+
---
+++ APPENDIX D/sed.sh
@@ -0,0 +1,15 @@
+# Sed is a stream editor. It's a good option if you want edit text files directly from the CLI.
+# Here's how i would print all the files that do not have "#" prefixed as their first Character.
+
+sed -e '/^#/d' host.deny | tail 
+
+# This is how i changed the default retry session for the fail2ban program with sed.
+# The default retry session is set to 1h
+
+sed -i "s/bantime      = 1h/ bantime      = 10h/g" /etc/fail2ban/jail.conf
+# Spaces are very important in sed. 
+
+# Here's how i would change the first character of a users name with sed. Let's assume usernames are stored in a names.txt file.
+
+sed -i "s/joseph/Joseph/g" names.txt 
+# The -i flag is important because if it's not appended before the command, it will temporary save the changes in memory.
